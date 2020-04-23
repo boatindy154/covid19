66781ab33acfa599524d0ebeccefbad74fa9cfd7
boatindy154 <boatindy154@gmail.com>
2020-04-23 16:29:35 +0700

    update survey

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 $(function() {
 
     var url = "https://pomber.github.io/covid19/timeseries.json";
-    $.getJSON(url, function(result) {
-        var no = 1;
-
-
-        for (var country in result) {
-
-            var row = `<tr>
-        <th scope="row">${no}</th>
-        <td><a href="country.html?country=${country}">${country}</a></td>
-
-
-        </tr>`;
-            $("#data").append(row);
-            no++;
-        }
-
-    });
+    var nowConfirmed = selectedCountry[(selectedCountry.length) - 1].confirmed;
+    var nowDeaths = selectedCountry[(selectedCountry.length) - 1].deaths;
+    var nowRecovered = selectedCountry[(selectedCountry.length) - 1].recovered;
+    $("#nowConfirmed").append(nowConfirmed);
+    $("#nowDeaths").append(nowDeaths);
+    $("#nowRecovered").append(nowRecovered);
 
 });
